22a8a4895db3075d847b2f2949b7bb6e9830f149
Michael Chong <daohanchong@gmail.com>
2024-03-23 08:12:01 -0600

    publicHeadersPath

--- Package.swift
+++ Package.swift
@@ -19,7 +19,8 @@ let package = Package(
         .target(
             name: "MMWormhole",
             dependencies: [],
-            path: "Source"
+            path: "Source",
+            publicHeadersPath: "."
         )
     ]
 )
